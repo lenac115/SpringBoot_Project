40242b0895d79407b36bd011c2de4a269163ef68
st-minju <kimminju0115@gmail.com>
2024-07-29 18:32:13 +0900

    :sparkles: 공지사항 출력
    #10

--- src/main/java/com/springProject/repository/PostsRepository.java
+++ src/main/java/com/springProject/repository/PostsRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springProject.entity.Posts;
@@ -11,6 +12,6 @@ import com.springProject.entity.Posts;
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long>, PostsRepositoryCustom {
 
-  // @Query("select p from Posts p where p.isNotice = true order by p.created_at DESC limit 5")
-  List<Posts> getFirst5ByIsNoticeTrueOrderByCreated_atDesc();
+  @Query("select p from Posts p where p.isNotice = true order by p.created_at DESC limit 5")
+  List<Posts> getNoticeFive();
 }
