38bc7a0d2f20d020cf2d815c294e42c2282be815
seongyeon.kim <ttt0342@naver.com>
2024-07-30 09:28:52 +0900

    게시판 생성, 수정, 삭제
    #17

--- src/main/java/com/springProject/entity/Users.java
+++ src/main/java/com/springProject/entity/Users.java
@@ -1,19 +1,56 @@
 package com.springProject.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Users {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name = "login_id")
+	private String loginId;
+
+	@Column
+	private String password;
+
+	@Column
+	private String name;
+
+	@Column
+	private String nickname;
+
+	@Column
+	private String email;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Column(nullable = false)
+	private Boolean activated;
+
+	@OneToMany(mappedBy = "users", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Comments> comments = new ArrayList<>();
+
+	@Enumerated(EnumType.STRING)
+	private UserAuth auth = UserAuth.user;
+
+	public enum UserAuth {
+		user, admin, stop
+	}
 }
--- src/main/java/com/springProject/repository/CommentsRepository.java
+++ src/main/java/com/springProject/repository/CommentsRepository.java
@@ -1,9 +1,19 @@
 package com.springProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springProject.entity.Comments;
 
+import java.util.List;
+
 @Repository
-public interface CommentsRepository extends JpaRepository<Comments, Long> {}
+public interface CommentsRepository extends JpaRepository<Comments, Long> {
+
+    @Query("select c from Comments c " +
+            "left join fetch c.parent " +
+            "where c.posts.id = :id " +
+            "order by c.parent.id asc nulls first, c.createdAt asc")
+    List<Comments> findByPostId(Long id);
+}
