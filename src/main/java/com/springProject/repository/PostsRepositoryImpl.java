ae5c1bcb12f38d7c7bbf47407855d1dde4a7df19
st-minju <kimminju0115@gmail.com>
2024-08-05 11:13:39 +0900

    게시글 전체보기 페이지 수정
    #19

--- src/main/java/com/springProject/repository/PostsRepositoryImpl.java
+++ src/main/java/com/springProject/repository/PostsRepositoryImpl.java
@@ -6,14 +6,18 @@ import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.querydsl.core.types.dsl.StringPath;
 import com.springProject.dto.PostsDto;
 import com.springProject.utils.ConvertUtils;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -49,24 +53,75 @@ public class PostsRepositoryImpl implements PostsRepositoryCustom {
 
 		OrderSpecifier order = getOrder(sortBy);
 
-		log.info("category = {}, location = {}, star = {}, hashtags = {}, startdate = {}, enddate = {}, sortBy = {}",
-			searchData.getCategory(), searchData.getLocation(), searchData.getStar(), searchData.getHashtag(),
-			searchData.getStartDate(), searchData.getEndDate(), sortBy);
+		Map<StringPath, String> map = new HashMap<>();
+		map.put(posts.title, keyword);
+		map.put(posts.body, keyword);
+		map.put(posts.category, category);
+		map.put(posts.location, location);
+		map.put(posts.hashtags, hashtag);
+
+		log.info("keyword = {}, category = {}, location = {}, star = {}, hashtags = {}, startdate = {}, enddate = {}, sortBy = {}",
+			searchData.getKeyword(), searchData.getCategory(), searchData.getLocation(), searchData.getStar(), searchData.getHashtag(),
+			start, end, sortBy);
 
 
 		// 실행할 쿼리 정의
+		// 공지사항은 아닌 게시물 중 조건에 맞는 게시물 검색
 		JPAQuery<Posts> query = queryFactory
 			.selectFrom(posts)
 			.where(posts.isNotice.isFalse()
-				.and((posts.star.goe(star))
-				.and(betweenDate(start, end))
-				.or((containsKeyword(keyword))
-				.or(eqCategory(category))
-				.or(eqLocation(location))
-				.or(containsHashtag(hashtag)))))
+				.and(
+					posts.star.goe(star)
+					.and(betweenDate(start, end))
+					))
 			.orderBy(order);
 
-		long totalCount = query.stream().count(); // 쿼리 결과가 총 몇개인지 확인
+		// keyword -> or 조건이나 and 조건 앞에 오는 값은 null일 수가 없으므로 확인해주기
+		// 다만, or이나 and 조건 뒤에 오는 값이 null이면 무시
+		if (StringUtils.hasText(keyword)) {
+			query = query
+				.where(containWord(posts.title, keyword) // 일치하는 제목 혹은 내용을 포함하는 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.body, keyword))
+					.or(isEqual(posts.category, category)) // 일치하는 카테고리를 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(isEqual(posts.location, location)) // 일치하는 위치 값을 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(isEqual(posts.hashtags, hashtag)) // 일치하는 해시태그를 포함하는 데이터 찾기 -> 대소문자 구분 X
+				)
+				.orderBy(order);
+		}
+		else if(StringUtils.hasText(category)) {
+			query = query
+				.where(isEqual(posts.category, category) // 일치하는 카테고리를 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.title, keyword)) // 일치하는 제목 혹은 내용을 포함하는 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.body, keyword))
+					.or(isEqual(posts.location, location)) // 일치하는 위치 값을 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(isEqual(posts.hashtags, hashtag)) // 일치하는 해시태그를 포함하는 데이터 찾기 -> 대소문자 구분 X
+				)
+				.orderBy(order);
+		}
+		else if(StringUtils.hasText(location)) {
+			query = query
+				.where(isEqual(posts.location, location) // 일치하는 위치 값을 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.title, keyword)) // 일치하는 제목 혹은 내용을 포함하는 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.body, keyword))
+					.or(isEqual(posts.category, category)) // 일치하는 카테고리를 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(isEqual(posts.hashtags, hashtag)) // 일치하는 해시태그를 포함하는 데이터 찾기 -> 대소문자 구분 X
+				)
+				.orderBy(order);
+		}
+		else if(StringUtils.hasText(hashtag)) {
+			query = query
+				.where(isEqual(posts.hashtags, hashtag) // 일치하는 해시태그를 포함하는 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.title, keyword)) // 일치하는 제목 혹은 내용을 포함하는 데이터 찾기 -> 대소문자 구분 X
+					.or(containWord(posts.body, keyword))
+					.or(isEqual(posts.category, category)) // 일치하는 카테고리를 가진 데이터 찾기 -> 대소문자 구분 X
+					.or(isEqual(posts.location, location)) // 일치하는 위치 값을 가진 데이터 찾기 -> 대소문자 구분 X)
+				)
+				.orderBy(order);
+		}
+
+
+		// 출력될 쿼리 결과가 총 몇 개인지 확인 -> 게시물 개수 확인 해서 페이징
+		long totalCount = query.stream().count();
 
     	query = query.offset(pageable.getOffset()) // pageable에 설정된 값으로 몇번째 데이터 부터 보여줄 지가 정의됨
 					.limit(pageable.getPageSize()); // offset 부터 몇 개의 데이터를 출력될 지가 정의됨
@@ -90,66 +145,38 @@ public class PostsRepositoryImpl implements PostsRepositoryCustom {
 		return order;
 	}
 
-	// 일치하는 제목 혹은 내용을 포함하는 데이터 찾기 -> 대소문자 구분 X
-	private BooleanExpression containsKeyword(String keyword) {
-		if(keyword != null) {
-			return (Expressions.stringTemplate(
-					"LOWER({0})", posts.title)
-				.contains(Expressions.stringTemplate(
-					"LOWER({0})",keyword)))
-				.or(Expressions.stringTemplate(
-					"LOWER({0})", posts.body)
-				.contains(Expressions.stringTemplate(
-					"LOWER({0})",keyword)));
-			}
-		return (posts.title.contains(posts.title)).or(posts.body.contains(posts.body));
+
+	// 일치 확인 메서드
+	private static BooleanExpression isEqual(StringPath tableData, String searchData) {
+		return StringUtils.hasText(searchData) ?
+				Expressions.stringTemplate(
+						"LOWER({0})", tableData)
+					.eq(Expressions.stringTemplate(
+						"LOWER({0})", searchData)) : null;
 	}
 
-
-	// 일치하는 카테고리를 가진 데이터 찾기 -> 대소문자 구분 X
-	private BooleanExpression eqCategory(String category) {
-		if( category != null)
-
-			return Expressions.stringTemplate(
-					"LOWER({0})", posts.category)
-				.eq(Expressions.stringTemplate(
-					"LOWER({0})",category));
-		return posts.category.eq(posts.category);
-	}
-
-	// 일치하는 위치 값을 가진 데이터 찾기 -> 대소문자 구분 X
-	private BooleanExpression eqLocation(String location) {
-		if(location != null)
-			return Expressions.stringTemplate(
-					"LOWER({0})", posts.location)
-				.eq(Expressions.stringTemplate(
-					"LOWER({0})",location));
-		return posts.location.eq(posts.location);
-	}
-
-	// 일치하는 해시태그를 포함하는 데이터 찾기 -> 대소문자 구분 X
-	private BooleanExpression containsHashtag(String hashtag) {
-		if(hashtag != null)
-			return Expressions.stringTemplate(
-					"LOWER({0})", posts.hashtags)
-				.contains(Expressions.stringTemplate(
-					"LOWER({0})",hashtag));
-		return posts.hashtags.contains(posts.hashtags);
+	// 포함 확인 메서드
+	private static BooleanExpression containWord(StringPath tableData, String searchData) {
+		return StringUtils.hasText(searchData) ?
+				Expressions.stringTemplate(
+						"LOWER({0})", tableData)
+					.contains(Expressions.stringTemplate(
+						"LOWER({0})", searchData)) : null;
 	}
 
 	// 시작날짜부터 끝나는날짜 사이에 생성된 게시물 불러오기
 	private BooleanExpression betweenDate(Timestamp start, Timestamp end) {
 
-		return posts.updated_at.between(start, end);
+		return posts.created_at.between(start, end);
 
 	}
 
 	// 날짜가 String으로 들어오기 때문에 TimeStamp 값으로 바꿔줘야함
 	private static Timestamp stringToTimeStamp(String point, String dateString) {
 		//  String to Timestamp
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		dateFormat.setLenient(false);// 날짜와 시간을 엄격하게 확인
-		if(dateString != null) {
+		if(StringUtils.hasText(dateString)) {
 			try {
 				Date stringToDate = dateFormat.parse(dateString);
 				Timestamp stringToTimestamp = new Timestamp(stringToDate.getTime());
@@ -157,18 +184,8 @@ public class PostsRepositoryImpl implements PostsRepositoryCustom {
 				return stringToTimestamp;
 			} catch (ParseException e) {
 				e.printStackTrace();
-
-				// 시작점이 지정 안되었으면 첫번째 게시물부터 출력해야함
-				if (point.equals("start"))
-					return Timestamp.valueOf("1900-01-01 00:00:00.000");
-
-					// 끝 점이 지정 안되었으면 지금까지 게시된 게시물까지 출력해야함
-				else
-					return new Timestamp(System.currentTimeMillis());
 			}
-
 		}
-
 		// 시작점이 지정 안되었으면 첫번째 게시물부터 출력해야함
 		if (point.equals("start"))
 			return Timestamp.valueOf("1900-01-01 00:00:00.000");
@@ -176,6 +193,6 @@ public class PostsRepositoryImpl implements PostsRepositoryCustom {
 		// 끝 점이 지정 안되었으면 지금까지 게시된 게시물까지 출력해야함
 		else
 			return new Timestamp(System.currentTimeMillis());
-
 	}
+
 }
--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,9 +1,63 @@
+// 현재 URI 파싱
 var parsingURI = location.search.split("page")[0];
 
+// 정렬에 연결할 URI 작업
+const sorting = document.querySelectorAll("#sorting option");
+
+sorting.forEach(sort => {
+    var optionId = sort.getAttribute("id");
+    sort.addEventListener("click", function (e) {
+        window.location.href = parsingURI + "sort="+optionId+"&page=1";
+    })
+
+})
+
+// 페이지 이동에 연결할 URI 작업
 const pageContainer = document.querySelectorAll('.move-page');
 
 for (let i = 0; i < pageContainer.length; i++) {
     var moveURI = parsingURI+"page"+pageContainer[i].href.split("page")[1];
     pageContainer[i].href = moveURI;
-    console.log(moveURI);
+}
+
+// select에서 선택된 값이 있으면 색상 변경
+const selectBoxElements = document.querySelectorAll("select");
+for (let i = 0; i < selectBoxElements.length; i++) {
+    selectBoxElements[i].childNodes.forEach(child => {
+        child.addEventListener("selected", function (event) {
+            if(child !== selectBoxElements[i].firstChild) {
+                selectBoxElements[i].style.color = "#171A1FFF";
+            }
+        })
+    })
+}
+
+
+// 공지사항 열고 닫기
+const arrowUp = document.querySelector("#up");
+const arrowDown = document.querySelector("#down");
+const noticeList = document.querySelector("#notice-list");
+
+arrowUp.addEventListener("click", () => {
+    arrowUp.style.visibility = "hidden";
+    arrowDown.style.visibility = "visible";
+
+    noticeList.classList.toggle("notice-list-hidden");
+});
+
+arrowDown.addEventListener("click", () => {
+    arrowDown.style.visibility = "hidden";
+    arrowUp.style.visibility = "visible";
+
+    noticeList.classList.toggle("notice-list-hidden");
+
+});
+
+// checkbox 하나만 선택되게 하기 = radio 버튼 + 선택 취소
+const starTags = document.querySelectorAll("#rating li input");
+
+function checkDuplicate(chk) {
+    starTags.forEach(starTag => {
+        if(starTag !== chk) {starTag.checked = false;}
+    })
 }
